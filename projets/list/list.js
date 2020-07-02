ceb7f31881717ee3fa85ced2a876e60fff85fd8b
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-07-01 23:40:11 -0400

    style

--- projets/list/list.js
+++ projets/list/list.js
@@ -14,24 +14,24 @@ clear.addEventListener('click', supprimerElement);
 document.addEventListener('DOMContentLoaded', displayStorage);
 
 
-function ajouterElement(event){
+function ajouterElement(event) {
     event.preventDefault();
-    let  inputValue = input.node;
-    if(inputValue !== "" ){
-      showAction(addItemsAction, `${inputValue} ajouer à la liste`, true);
-      createItem(inputValue);
-      updateStorage(inputValue)
+    let inputValue = input.node;
+    if (inputValue !== "") {
+        showAction(addItemsAction, `${inputValue} ajouer à la liste`, true);
+        createItem(inputValue);
+        updateStorage(inputValue)
     }
 }
 
-function showAction(element, text, inputValue){
+function showAction(element, text, inputValue) {
     // si le champ est rempli on alert succès et on appele la fonctionqui ajouter un element
-    if (inputValue === true){
-        element.classList.add('succes');
+    if (inputValue === true) {
+        element.classList.add('success');
         element.innerHtml = text;
         input.value = '';
         setTimeout(function () {
-            element.classList.remove('succes')
+            element.classList.remove('success')
         }, 3000)
     }
     // Sinon on alert 
@@ -42,11 +42,11 @@ function showAction(element, text, inputValue){
         setTimeout(function () {
             element.classList.remove('alert')
         }, 3000)
-        
+
     }
 }
 
-function createItem(inputValue){
+function createItem(inputValue) {
     let parent = document.createElement('div');
     parent.classList.add('grocery-item');
 
@@ -58,14 +58,26 @@ function createItem(inputValue){
     list.appendChild(parent);
 }
 
-function updateStorage(){
-    
-}
-
-function retirerElement(){
+function updateStorage() {
 
 }
 
-function supprimerElement(){
+function retirerElement(event) {
+    event.preventDefault();
+    let link = event.target.parentElement;
+    if (link.classList.contains('grocery-item__link')) {
+        let text = link.previousElement.parentElement;
+        editStorage(text);
+
+        list.removeChild(groceryItem)
+        showAction(displayItemsAction, `${text} Retirer de la liste`, true)
+    }
+}
+
+function supprimerElement() {
+
+}
+
+function displayStorage() {
 
 }
