98bd287284f0bfb03ab50fcc1b38fd2687e0f460
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-07-03 02:03:55 -0400

    add js

--- projets/list/list.js
+++ projets/list/list.js
@@ -7,24 +7,49 @@ const clear = document.querySelector('.displayItems-clear');
 
 
 
-submit.addEventListener('click', ajouterElement);
-list.addEventListener('click', retirerElement);
+submit.addEventListener('click', addArticle);
+/* list.addEventListener('click', retirerElement);
 clear.addEventListener('click', supprimerElement);
 
-document.addEventListener('DOMContentLoaded', displayStorage);
+document.addEventListener('DOMContentLoaded', displayStorage); */
 
 
-function ajouterElement(event) {
+function addArticle(event) {
     event.preventDefault();
-    let inputValue = input.node;
+    let inputValue = input.value;
     if (inputValue !== "") {
-        showAction(addItemsAction, `${inputValue} ajouer à la liste`, true);
-        createItem(inputValue);
-        updateStorage(inputValue)
+        let parent = document.createElement('div');
+        parent.classList.add('grocery-item');
+        parent.innerHtml = `<h4 class="grocery-item__title">${inputValue}</h4>
+        <a href="#" class="grocery-item__link">
+         <i class="far fa-trash-alt"></i>
+        </a>`;
+        list.appendChild(parent);
+        console.log(inputValue);
+        showAction
     }
 }
 
-function showAction(element, text, inputValue) {
+function showAction(element, text, value) {
+    if (value === true) {
+     element.classList.add('success');
+     element.innerText = text;
+     input.value = '';
+     setTimeout(function () {
+      element.classList.remove('success')
+     }, 3000)
+    }
+    else {
+     element.classList.add('alert');
+     element.innerText = text;
+     input.value = '';
+     setTimeout(function () {
+      element.classList.remove('alert')
+     }, 3000)
+    }
+   }
+
+/* function showAction(element, text, inputValue) {
     // si le champ est rempli on alert succès et on appele la fonctionqui ajouter un element
     if (inputValue === true) {
         element.classList.add('success');
@@ -47,15 +72,7 @@ function showAction(element, text, inputValue) {
 }
 
 function createItem(inputValue) {
-    let parent = document.createElement('div');
-    parent.classList.add('grocery-item');
 
-    parent.innerHtml = `<h4 class="grocery-item__title">${value}</h4>
-    <a href="#" class="grocery-item__link">
-     <i class="far fa-trash-alt"></i>
-    </a>`;
-
-    list.appendChild(parent);
 }
 
 function updateStorage() {
@@ -80,4 +97,4 @@ function supprimerElement() {
 
 function displayStorage() {
 
-}
+} */
